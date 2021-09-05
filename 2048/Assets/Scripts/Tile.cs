d55ffb270e4cfb303f72e00a923dff0d34ed8427
nluechee <52709519+nluechee@users.noreply.github.com>
2021-09-04 21:45:07 -0400

    Finishing the game - mostly (#3)
    * Added some colour and animation
    
    Added colour changes
    Added a punch animation to spawning tiles
    Added a way to lose... kinda
    
    * Finished the game
    
    Reworked the loss detection.
    Added a score counter.
    Added a game over message and a play again button.

--- 2048/Assets/Scripts/Tile.cs
+++ 2048/Assets/Scripts/Tile.cs
@@ -9,38 +9,24 @@ public class Tile : MonoBehaviour
     public Vector2 currentCoord;
     public Vector2 previousCoord;
     public bool merge;
+    public bool destroy;
+
+    public Image tileColor;
     public Text numberText;
     public int value;
+    [SerializeField] Color[] colorScale;
     
     void Awake()
     {
         initValue();
         numberText.text = value.ToString();
+        updateColor();
     }
 
-
-    // Update is called once per frame
-    void Update()
+    public void punch()
     {
-    }
-
-    public Tween move(float distance, Direction inputDir)
-    {
-        Tween tileTween;
-        switch (inputDir)
-        {
-            case Direction.up:
-            case Direction.down:
-                tileTween = transform.DOMoveY(transform.position.y + distance,0.1f, true);
-                return tileTween;
-
-            case Direction.left:
-            case Direction.right:
-                tileTween = transform.DOMoveX(transform.position.x + distance, 0.1f, true);
-                return tileTween;
-            default:
-                return null;
-        }
+        transform.DOPunchScale(new Vector3(0.2f,0.2f,0.2f), 0.1f, 0, 0f);
+        return;
     }
 
     private void initValue()
@@ -57,14 +43,19 @@ public class Tile : MonoBehaviour
         
     }
 
-    public void destroyTile()
-    {
-        Destroy(gameObject);
-    }
-
     public void updateValue()
     {
         value *= 2;
         numberText.text = value.ToString();
+    }
+
+    public void updateColor()
+    {
+        int colorIndex = (int)Mathf.Clamp((Mathf.Log(value, 2) - 1), 0, 14);
+        tileColor.DOColor(colorScale[colorIndex],0);
+        if (colorIndex > 1)
+        {
+            numberText.DOColor(new Color(255, 255, 255), 0);
+        }
     }
 }
